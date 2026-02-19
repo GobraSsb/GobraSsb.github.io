85cc0f27468ab0136272efe5415eb706de320f37
Gabriel <gabogabbriel@gmail.com>
2026-02-19 11:06:21 +0100

    javaescrip

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-let nombre="Pepe"
+/*let nombre="Pepe"
 let edad=22
 let curso="Cocina con Aznar"
 
-console.log("te llamas" + nombre + " y tu edad es: "+edad+ " Y estudias "+ curso)
+console.log("te llamas" + nombre + " y tu edad es: "+edad+ " Y estudias "+ curso)*/
+
+function numeroAleatorio(max){
+    let numero=Math.floor(Math.random()max)+1)
+    return numero
+}
+
+let numero = numeroAleatorio(49)
+console.log(numero)
