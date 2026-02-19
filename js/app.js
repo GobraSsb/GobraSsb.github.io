bf06c7b71d2ae53d71e2cd46ee049d13ed3885fc
Gabriel <gabogabbriel@gmail.com>
2026-02-19 11:34:34 +0100

    numeroaleatorio

--- js/app.js
+++ js/app.js
@@ -5,9 +5,16 @@ let curso="Cocina con Aznar"
 console.log("te llamas" + nombre + " y tu edad es: "+edad+ " Y estudias "+ curso)*/
 
 function numeroAleatorio(max){
-    let numero=Math.floor(Math.random()max)+1)
+    let numero=Math.floor(Math.random()* max)+1;
     return numero
 }
-
+let numero=0
+let cantidad =0
+while (cantidad<6){
+cantidad++
 let numero = numeroAleatorio(49)
 console.log(numero)
+}
+
+
+
