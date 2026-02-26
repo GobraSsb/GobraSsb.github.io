f9ec1460a38368b5afc456b442deb2f3cd778cc6
Gabriel <gabogabbriel@gmail.com>
2026-02-26 10:23:27 +0100

    cosicas

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let curso="Cocina con Aznar"
 
 console.log("te llamas" + nombre + " y tu edad es: "+edad+ " Y estudias "+ curso)*/
 
-function numeroAleatorio(max){
+/*function numeroAleatorio(max){
     let numero=Math.floor(Math.random()* max)+1;
     return numero
 }
@@ -14,7 +14,19 @@ while (cantidad<6){
 cantidad++
 let numero = numeroAleatorio(49)
 console.log(numero)
+}*/
+let alfabeto=["abcdefghijklmnñopqrstuvwxyz"]
+let frase="pimientoz"
+let paso=3
+
+for(let i=0; i<frase.length; i++){
+    let posicion=alfabeto.indexOf(frase[i])%alfabeto.length
+    let letraCifrada=alfabeto[posicion+paso]
+    console.log("la letra: "+ frase[i]+ " está en la posición: "
+        +posicion+" y es: "+letraCifrada
+    )
 }
 
+console.log(alfabeto[6])
 
 
