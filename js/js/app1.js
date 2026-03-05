a45b7196c979ddc12277c125c7dfeb146a82b077
Gabriel <gabogabbriel@gmail.com>
2026-03-05 13:15:41 +0100

    guapisimo

--- js/js/app1.js
+++ js/js/app1.js
@@ -2,12 +2,14 @@
 let num1=document.getElementById("numero1")
 let num2 = document.getElementById("numero2")
 let boton=document.getElementById("run")
+let salida = document.getElementById("salida")
 
 //eventos
 boton.addEventListener("click", (e)=> {
    console.log("Me han pulsado: " + (parseInt(num1.value) + parseInt(num2.value)))
    for(let i=0; i<parseInt(num2.value); i++){
-console.log(num1.value)
+      TEXTO=num1.value+"<br>"
+salida.innerHTML=TEXTO
 }
 })
 
